571b46084f72f17c0e858d38783777d06de2e4f7
suaji <suaji86@gmail.com>
2015-11-11 14:44:24 +0800

    no message

---
+++ plugin.video.muvemalay/default.py
@@ -0,0 +1,345 @@
+import sys
+import xbmcgui
+import xbmcplugin
+
+addon_handle = int(sys.argv[1])
+
+xbmcplugin.setContent(addon_handle, 'movies')
+
+url = 'https://copy.com/Bg3yctJnCuBD10rU/%5BPENCURIMOVIE.PW%20%5D%20Jwanita.2015.720P.PPVRiP.AC3.PFT.mkv'
+li = xbmcgui.ListItem('Jwanita (2015) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/jwanita_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/WctEk9kfl9uKOqwJ/Malay/%5BPENCURIMOVIE.PW%5D%20Kapsul.2015.DVDRiP.576P.AC3.PFT.mkv'
+li = xbmcgui.ListItem('Kapsul (2015) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/kapsul_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/2Tqz9NFKaCXnDG1J/%5BPENCURIMOVIE.PW%5D%20Usop.Wilcha.Menghonjang.Makhluk.Muzium.2015.DVDRiP.576P.AC3.PFT.mkv'
+li = xbmcgui.ListItem('Usop Wilcha Menghonjang Makhluk Muzium (2015) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/usopwilcha_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/KzRs6nfDlsHArLDZ/%5BPENCURIMOVIE.PW%5D%20Bravo.5.2015.720P.mkv'
+li = xbmcgui.ListItem('BRAVO (2015) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/bravo_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/si4haiPLp4lUWGoy/Jejak.Warriors.2015.DVDRiP.576P.AC3.PFT.mkv'
+li = xbmcgui.ListItem('Jejak Warriors (2015) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/jejakwira_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/aE1qZBxKZKtq1Uf3/Bukit.Kepong.HD.2015.PPVRiP.720P.AC3.PFT.mkv'
+li = xbmcgui.ListItem('Bukit Kepong (2015) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/bukitkepong_p.jpg')
+li.setProperty('fanart_image', 'http://myiptv.site40.net/kodi/fanart/bukitkepong_f.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/FvtghSQjjAK0Zq34/CCTV.2015.PPVRiP.720P.AAC.PFT.mkv'
+li = xbmcgui.ListItem('CCTV (2015) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/cctv_p.png')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/dGlLIEdTVlWCyYYg/%5BPENCURIMOVIE.PW%5D%20Misi.Rayyan.Dan.Danish.2015.DVDRiP.576P.AC3.PFT.mkv'
+li = xbmcgui.ListItem('Misi Rayyan dan Danish (2012) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/misirayyan_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/2Tqz9NFKaCXnDG1J/%5BPENCURIMOVIE.PW%5D%20Nota.2015.DVDRiP.576P.AC3.PFT.mkv'
+li = xbmcgui.ListItem('Nota (2012) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/nota_p.png')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/VPkQERcSH6EjruSR/Suamiku.Jatuh.Dari.Langit.2015.720P.UPSCALED.PPVRiP.AAC.x264.PFT.mkv'
+li = xbmcgui.ListItem('Suamiku Jatuh Dari Langit (2015) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/suamilangit_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/ZLCJfgFmpLa1YPeg/Gangsterock.Kasi.Sengat.2015.720P.UPSCALED.PPVRiP.AAC.x264.PFT.mkv'
+li = xbmcgui.ListItem('Gangsterock Kasi Sengat (2015) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/gangsterock_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/wRcqblpSFaC4JI8e/Banting.2015.576P.PPVRIP.AAC.PFT.mkv'
+li = xbmcgui.ListItem('Banting (2015) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/banting_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/XMdyGriWC3k8Oi69/%5BPENCURIMOVIE.PW%5D%20Hantu.Wefie.2015.HDTV.720P.mp4'
+li = xbmcgui.ListItem('Hantu Wefie (2015) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/hantuwefie_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/nJKJlsJWpqdpEQNW/Isyarat.Cinta.2015.TVRiP.720P.AAC.PFT.mkv'
+li = xbmcgui.ListItem('Isyarat Cinta (2015) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/isyaratcinta_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/RGDGfnt4lbghFvQM/Suami.Aku.Ustaz.2015.DVDRiP.576P.AAC.PFT.mkv'
+li = xbmcgui.ListItem('Suami Aku Ustaz (2015) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/suamiustaz_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/or0WlC9cE1HcmwKU/Dia.2015.DVDRiP.576P.AAC.PFT.mkv'
+li = xbmcgui.ListItem('Dia (2015) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/dia_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/kdXsk7xX86HJGRjM/Jendela.2015.DVDRiP.576P.AAC.PFT.mkv'
+li = xbmcgui.ListItem('Jendela (2015) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/jendela_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/3mBmhdsfw0QxealF/Ophilia.2014.WEB-DL.720P.AAC.PFT.mkv'
+li = xbmcgui.ListItem('Ophilia (2015)')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/ophilia_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/WZIpuTMHkfUCz0S0/Jalan.Puncak.Alam.2015.576P.DVDRiP.AAC.PFT.mkv'
+li = xbmcgui.ListItem('Jalan Puncak Alam (2015) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/jalanpuncakalam_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/PAvvRoaNDWfS9qyN/Pilot.Cafe.2015.PPVRIP.480P.AAC.PFT.mkv'
+li = xbmcgui.ListItem('Pilot Cafe (2015) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/pilotcafe_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/gC2XttnPiSf0EduQ/To.Know.Malaysia.Is.To.Laugh.2014.480.AAC.PPVRIP.PFT.mkv'
+li = xbmcgui.ListItem('To Know Malaysia is To Laugh (2014) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/laugh_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/z6KlwXoB9k0fXxVY/Abang.Lejen.2015.DVDRip.x264.AAC.mkv'
+li = xbmcgui.ListItem('Abang Lejen (2015) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/abglejen_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/1pX17zin3IKJf7mq/Hantu.Bungkus.Ikat.Tepi.2015.480P.AAC.PFT.mkv'
+li = xbmcgui.ListItem('Hantu Bungkus Ikat Tepi (2015) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/hantubungkusikattepi_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/08n7tWDMbjF7ErDO/Jengka.2015.480P.PPVRIP.PFT.mkv'
+li = xbmcgui.ListItem('Jengka (2015) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/jengka_p.JPG')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/xWlbWXMpqOzHV0zr/Werewolf.Dari.Bangladesh.2015.480P.AAC.PFT.mkv'
+li = xbmcgui.ListItem('Werewolf Dari Bangladesh (2015) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/werewolfbangla_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/4q1NuZGT2KUSnvwo/Cicakman.3.2015.720P.AAC.PFT.mkv'
+li = xbmcgui.ListItem('Cicakman 3 (2015)')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/cicakman3_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/ZCdRmOS3PxHmFyb0/Selfie%20Suara%20Kubur%202015%20WEB-DL%20720p%20x264%20AC3.mkv'
+li = xbmcgui.ListItem('Selfie Suara Kubur (2015) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/selfiekubur_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/s2rywapTLRKJb4q3/Coverina.2015.PPVRIP.480P.AAC.PFT.mkv'
+li = xbmcgui.ListItem('Coverina (2015) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/coverina_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/n7N3JKJ6HkWXXpmz/Gudang.Kubur.2015.720P.AAC.PFT.mkv'
+li = xbmcgui.ListItem('Gudang Kubur (2015) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/gudangkubur_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/CFOArwCk7QjWZ4Xa/Bangunan.2015.480P.AAC.PPVRIP.PFT.mkv'
+li = xbmcgui.ListItem('Bangunan (2015) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/bangunan_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/7vPMAN2Lm6uP89Sj/Suamiku.Encik.Perfect.10.PPVRIP.720P.PFT.mkv'
+li = xbmcgui.ListItem('Suamiku Encik Perfect 10 (2015) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/suamiperfect10_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/3E6nS0DKn0NH7k2h/Puaka.Balai.Gombak.2015.PPVRIP.480P.AAC.PFT.mkv'
+li = xbmcgui.ListItem('Puaka Balai Gombak (2015)')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/puakabalai_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/A1dtEpKLjsPQiYWh/Villa%20Nabila%20%282015%29.mkv'
+li = xbmcgui.ListItem('Villa Nabila (2015) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/vilanabila_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/hUOjy7qdPwx8Stag/Rumah.Pusaka.Di.Simpang.Jalan.2015.PPVRip.x264.AC3.mkv'
+li = xbmcgui.ListItem('Rumah Pusaka Di Simpang Jalan (2015) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/rumahpuaka_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/NeJlJOeAsIRjnan9/Misi.Tawan.Baby.2014.WEB-DL.720P.AAC.PFT.mkv'
+li = xbmcgui.ListItem('Misi Tawan Baby (2014) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/misibaby_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/uXb0AMd3lvQr6qGc/Hantu.Bukit.Cina.2014.SDTVRip.x264.AC3-PFT-Kakisharing.com.mkv'
+li = xbmcgui.ListItem('Hantu Bukit Cina (2014) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/hantubukitcina_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/3raJs2KCnRKd1E3x/Fobia.Malam.Pertama.2014.HDTV.720p.x264.AC3.mkv'
+li = xbmcgui.ListItem('Fobia Malam Pertama (2014) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/fobiamlm_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/MMSfrOCtE3tkLJAe/Kasut%20Ku%20Kusut%20%282014%29%20480P%20PPVRIP%20AAC%20PENCURIMOVIE.mkv'
+li = xbmcgui.ListItem('Kasut Ku Kusut (2014) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/kasutkitai_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/eTXNeFcrV6X4Ltrw/Manisnya%20Cinta%20Di%20Cappadoccia%20%282014%29%20HDTV%20720P%20AAC%20PENCURIMOVIE.mkv'
+li = xbmcgui.ListItem('Manisnya Cinta Di Cappadocia (2014) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/cintacappadocia_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/rrFDjWYPm2Oy1yok/Rentap%20%282014%29%20480P%20PPVRIP%20AAC%20PENCURIMOVIE.mkv'
+li = xbmcgui.ListItem('Rentap (2014) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/rentap_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/MecvXbpSQ8SlYMI0/Cerita%20Hantu%20Malaysia%20%282014%29%20PPVRIP%20480P%20AAC%20PENCURIMOVIE.mkv'
+li = xbmcgui.ListItem('Cerita Hantu Malaysia (2014) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/ceritahantu_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/h1kvnHgfBhV91Tt3/Terbaik%20Dari%20Langit%20%282014%29%20PPVRIP%20HDTV%20720P%20AAC%20PENCURIMOVIE.CC.mkv'
+li = xbmcgui.ListItem('Terbaik Dari Langit (2014) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/terbaiklangit_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/9uoLc8YOVn13sKRw/Kaki%20Kitai%20%282014%29.mkv'
+li = xbmcgui.ListItem('Kaki Kitai (2014) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/kitai_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/pueLjKGRnTv8LwOf/Kelibat.DVDRip.2014.PFT-Release.mkv'
+li = xbmcgui.ListItem('Kelibat (2014) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/kelibat_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/AcRDww0i29mGr7HE/Dendam.Orang.Mati.2014.DVDRip.downloadur.org.mkv'
+li = xbmcgui.ListItem('Dendam Orang Mati (2014) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/dendamorgmati_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/0rOlWWmtweHSYJL8/Misteri.Syirik.Tahyul.2014.DVDRip.mkv'
+li = xbmcgui.ListItem('Misteri Syirik Tahyul (2014) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/mistiksyirik_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/Sngq2BDxb0yMxWc9/Mamak.Cup.Cake.2014.DVDRip.downloadur.org.mkv'
+li = xbmcgui.ListItem('Mamak Cupcake (2014) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/mamak_p.JPG')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/I7PKOSe609krDFI0/Nasi.Tangas.2014.DVDRip.downloadur.org.mkv'
+li = xbmcgui.ListItem('Nasi Tangas (2014) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/nasitangas_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/SPf4l0M3HdeEEarv/Muka%20Surat%20Cinta.2014.DVDRip.downloadur.org.mkv'
+li = xbmcgui.ListItem('Muka Surat Cinta (2014) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/mukasurat_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/IXq3pqiEy2B4bRiI/Lagenda.Budak.Setan.3.Kasyah.2014.HDTV.720p.downloadur.org.mkv'
+li = xbmcgui.ListItem('Lagenda Budak Setan 3 (2014) HD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/budaksetan_3.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/4yrRRc9cOlwYbndU/Hantu%20Nan%20Sempit.2014.WEBRip.downloadur.org.mkv'
+li = xbmcgui.ListItem('Hantu Nan Sempit (2014) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/hantusempit_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/FzyAhOMi8mLAxGdM/Adnan.Sempit.Sawadikap.2014.DVDRip.downloadur.org.mkv'
+li = xbmcgui.ListItem('Adnan Sempit Sawadikap (2014) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/adnansawadikap_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/UiHVd4SrkwWSHzad/Cara.Mengundang.Hantu.2014.DVDRip.XviD.AC3.avi'
+li = xbmcgui.ListItem('Cara Mengundang Hantu (2014) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/caraundanghantu_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/6UPqwk31HMyCERdN/Ia.Wujud.2014.DVDRip.XviD.AC3.avi'
+li = xbmcgui.ListItem('Ia Wujud (2014) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/iawujud_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/EYb2ZyS7GjeL7AVn/Panik.2014.SDTVRip.XviD.AC3-LTTi.PFT-KakiSharing.com.avi'
+li = xbmcgui.ListItem('Panik (2014) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/panik_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/xjff0OFYBGoktgAX/Paku.Pontianak.2013.mkv'
+li = xbmcgui.ListItem('Paku Pontianak (2013) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/pakuponti_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/3ibnEPzHcAwNgmlR/Istanbul%20Aku%20Datang.jie.azz.avi'
+li = xbmcgui.ListItem('Istanbul Aku Datang (2012) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/istanbulaku_p.jpeg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/6Jun0wjkYvrMqjEN/Aku%20Terima%20Nikahnya.avi'
+li = xbmcgui.ListItem('Aku Terima Nikahnya (2012) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/akuterima_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/D4WEiHisvKd0hZLJ/Seram%20Sejuk.jie.azz.avi'
+li = xbmcgui.ListItem('Seram Sejuk (2012) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/seramsejuk_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/PC8377o9w6WtRLdx/Karak.avi'
+li = xbmcgui.ListItem('Karak (2011) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/karak_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/6welEGX3euYaQoDy/KHURAFAT.avi'
+li = xbmcgui.ListItem('Khurafat (2011) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/khurafat_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/tRpjq8EclBbCd12c/Senjakala.avi'
+li = xbmcgui.ListItem('Senjakala (2011) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/senjakala_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/7Dbr0dpKVFGHpXMT/Penunggu%20Istana.avi'
+li = xbmcgui.ListItem('Penunggu Istana (2011) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/penungguistana_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/TQkWNH2a8IhbxsqH/Seru.avi'
+li = xbmcgui.ListItem('Seru (2011) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/seru_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/19VdKhC3uYt2VRaz/Hantu%20Kak%20Limah%20Balik%20Rumah.avi'
+li = xbmcgui.ListItem('Hantu Kak Limah Balik Rumah (2010) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/hantulimah_p.png')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/PiP4P93zbbnPQcos/Jangan%20Pandang%20Belakang%20Congkak.avi'
+li = xbmcgui.ListItem('Jangan Pandang Belakang Congkak (2009) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/jpbc2.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'https://copy.com/c78LVpEu4LHIr4ZK/Jangan.Tegur.2009.DVDrip.reza6649.avi'
+li = xbmcgui.ListItem('Jangan Tegur (2009) SD')
+li.setIconImage('http://myiptv.site40.net/kodi/poster/jangantegur_p.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+xbmcplugin.endOfDirectory(addon_handle)
